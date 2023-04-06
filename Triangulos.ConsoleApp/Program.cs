febd1091fbfb6e7345a42758f3a98c08bdba1ac6
Alexandre Rech <alexandre.rech.lages@gmail.com>
2023-04-06 16:46:06 -0300

    Add project files.

---
+++ Triangulos.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    class TesteTriangulo
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo1 = new Triangulo(10, 10, 10);                         
+
+            string tipoTriangulo1 = triangulo1.ObterTipo();
+
+            Console.WriteLine(tipoTriangulo1); //Eqüilátero
+
+            Triangulo triangulo2 = new Triangulo(12, 10, 10);
+
+            string tipoTriangulo2 = triangulo2.ObterTipo();
+
+            Console.WriteLine(tipoTriangulo2); //Isósceles
+
+            Triangulo triangulo3 = new Triangulo(12, 14, 16);
+
+            string tipoTriangulo3 = triangulo3.ObterTipo();
+
+            Console.WriteLine(tipoTriangulo3); //Escaleno
+
+            Console.WriteLine("O numero de triangulos criados foram: " /*+ Triangulo.quantidadeTriangulos*/);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Triangulos.ConsoleApp/Triangulo.cs
@@ -0,0 +1,66 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    public class Triangulo
+    {
+        private int ladoX;
+        private int ladoY;
+        private int ladoZ;
+
+        public string Tipo;
+
+        private static int quantidadeTriangulos = 0;
+
+        public Triangulo(int ladoX, int ladoY, int ladoZ) //método constructor EXPLÍCITO
+        {
+            //todo triangulo que for criado deve ser escaleno
+            this.ladoX = ladoX;
+            this.ladoY = ladoY;
+            this.ladoZ = ladoZ;
+
+            quantidadeTriangulos++;
+        }
+
+        public string ObterTipo()
+        {            
+            string tipo;
+
+            //padrão null guard
+            if (LadoXInvalido() || LadoYInvalido() || LadoZInvalido())
+                tipo = "Invalido";
+
+            else if (ladoX == ladoY && ladoY == ladoZ)
+                tipo = "Eqüilátero";
+
+            else if (ladoX != ladoY && ladoY != ladoZ && ladoZ != ladoX)
+                tipo = "Escaleno";
+
+            else
+                tipo = "Isósceles";
+
+            return tipo;
+        }      
+
+        private bool LadoZInvalido()
+        {
+            bool ladoZInvalido = ladoZ > ladoX + ladoY;
+
+            return ladoZInvalido;
+        }
+
+        private bool LadoYInvalido()
+        {
+
+            bool ladoXInvalido = ladoX > ladoZ + ladoY;
+
+            return ladoXInvalido;
+        }
+
+        private bool LadoXInvalido()
+        {
+
+            bool ladoYInvalido = ladoY > ladoZ + ladoX;
+
+            return ladoYInvalido;
+        }
+    }
+}
